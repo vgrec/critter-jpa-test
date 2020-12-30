e9202ff824d1f570894e7394f1810c8bd7d983ac
vgrec <grec.vs@gmail.com>
2020-12-30 12:07:01 +0100

    Get owner pets

--- src/main/java/com/udacity/jdnd/course3/critter/CritterApplication.java
+++ src/main/java/com/udacity/jdnd/course3/critter/CritterApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Launches the Spring application. Unmodified from starter code.
+ *
+ * https://docs.spring.io/spring-data/jpa/docs/2.4.2/reference/html/#appendix.query.method.subject
+ * https://docs.spring.io/spring-data/jpa/docs/2.4.2/reference/html/#repositories.core-concepts
+ * https://www.baeldung.com/spring-data-derived-queries
+ *
  */
 @SpringBootApplication
 public class CritterApplication {
--- src/main/java/com/udacity/jdnd/course3/critter/service/PersonService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/PersonService.java
@@ -5,6 +5,7 @@ import com.udacity.jdnd.course3.critter.persistance.data.Owner;
 import com.udacity.jdnd.course3.critter.persistance.data.Pet;
 import com.udacity.jdnd.course3.critter.persistance.repository.EmployeeRepository;
 import com.udacity.jdnd.course3.critter.persistance.repository.OwnerRepository;
+import com.udacity.jdnd.course3.critter.persistance.repository.PetRepository;
 import com.udacity.jdnd.course3.critter.pet.PetDTO;
 import com.udacity.jdnd.course3.critter.user.CustomerDTO;
 import com.udacity.jdnd.course3.critter.user.EmployeeDTO;
@@ -13,15 +14,18 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
     private EmployeeRepository employeeRepository;
     private OwnerRepository ownerRepository;
+    private PetRepository petRepository;
 
-    public PersonService(EmployeeRepository employeeRepository, OwnerRepository ownerRepository) {
+    public PersonService(EmployeeRepository employeeRepository, OwnerRepository ownerRepository, PetRepository petRepository) {
         this.employeeRepository = employeeRepository;
         this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
     }
 
     public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
@@ -32,6 +36,12 @@ public class PersonService {
 
     public List<CustomerDTO> getAllCustomers() {
         List<Owner> owners = ownerRepository.findAll();
+
+        for (Owner owner : owners) {
+            List<Pet> ownersPets = petRepository.findPetsByCustomerId(owner.getId());
+            owner.setPets(ownersPets);
+        }
+
         List<CustomerDTO> customers = new ArrayList<>();
         for (Owner owner : owners) {
             customers.add(toCustomerDTO(owner));
@@ -83,8 +93,34 @@ public class PersonService {
         dto.setPhoneNumber(owner.getPhoneNumber());
         dto.setNotes(owner.getNotes());
 
+        if (owner.getPets() != null) {
+            List<PetDTO> ownerPets = new ArrayList<>();
+            for (Pet pet : owner.getPets()) {
+                ownerPets.add(toPetDTO(pet));
+            }
+            List<Long> petIds = ownerPets.stream().map(PetDTO::getId).collect(Collectors.toList());
+            dto.setPetIds(petIds);
+        }
+
         return dto;
     }
+
+    private PetDTO toPetDTO(Pet pet) {
+        PetDTO dto = new PetDTO();
+        dto.setId(pet.getId());
+        dto.setName(pet.getName());
+        dto.setType(pet.getType());
+        dto.setOwnerId(pet.getCustomerId());
+
+        // TODO: add other fields
+
+        if (pet.getOwner() != null) {
+            dto.setOwnerId(pet.getOwner().getId());
+        }
+
+        return dto;
+    }
+
 
     private Pet toPet(PetDTO dto) {
         Pet pet = new Pet();
