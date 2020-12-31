10dc566c7faf75d24f1132d176f83de87d7a7f17
vgrec <grec.vs@gmail.com>
2020-12-31 14:54:58 +0100

    Add comment to Person class

--- src/main/java/com/udacity/jdnd/course3/critter/persistance/data/Person.java
+++ src/main/java/com/udacity/jdnd/course3/critter/persistance/data/Person.java
@@ -2,6 +2,14 @@ package com.udacity.jdnd.course3.critter.persistance.data;
 
 import javax.persistence.*;
 
+/**
+ * Super class for {@link Customer} and {@link Employee}.
+ *
+ * The inheritance relationship it's not reflected at the persistence level.
+ * This has the effect of duplicating the ID and Name fields in both
+ * Customer and Employee tables. On the plus side, the generated schema
+ * is simpler, and for this example I chose simplicity first.
+ */
 @MappedSuperclass
 public class Person {
     @GeneratedValue
